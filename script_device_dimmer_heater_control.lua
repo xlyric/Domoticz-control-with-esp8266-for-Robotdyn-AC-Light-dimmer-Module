9ec5bd0595bcca0d926ef23a05f1e5161085d823
Cyril Poissonnier <6286087+xlyric@users.noreply.github.com>
2019-04-21 22:40:34 +0200

    Update script_device_dimmer_heater_control.lua
    add fuse security and alerte in logs

--- script_device_dimmer_heater_control.lua
+++ script_device_dimmer_heater_control.lua
@@ -10,6 +10,12 @@
 IP="192.168.1.75"
 IDXNAME="Relay délestage"
 
+-- soft fuse 
+fuse=2 -- in A 
+load=800 -- in watt
+
+valuemax=(2*220/800*100)
+
 -- main program 
 
 commandArray = {}
@@ -19,11 +25,19 @@ if (devicechanged[IDXNAME]) then
 		-- get Domoticz Dimmer information switch 
 	val_production = tonumber(otherdevices_svalues[IDXNAME])
 
+		if val_production > valuemax then
+			val_production = valuemax
+			commandArray[IDXNAME]='Set Level '..val_production
+		print("security mode")
+		end
+		
+
 		-- send information to esp8266 dimmer
 		commandArray['OpenURL']=IP..'/?POWER='..val_production
 		
 		-- log action
 		print("commande send to :"..val_production.."%")
+
 end
 	
 return commandArray

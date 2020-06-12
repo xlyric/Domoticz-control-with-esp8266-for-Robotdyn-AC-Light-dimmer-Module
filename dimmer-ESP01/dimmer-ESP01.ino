ec5b4c463285c7ab5d743c39cebe6cadf7773cec
Cyril Poissonnier <6286087+xlyric@users.noreply.github.com>
2020-06-12 18:35:02 +0200

    support pour ESP01

---
+++ dimmer-ESP01/dimmer-ESP01.ino
@@ -0,0 +1,507 @@
+/**************
+ *  numeric Dimmer  ( using robodyn dimmer =  
+ *  **************
+ *  Upgraded By Cyril Poissonnier 2020
+ *  FOR ESP01 
+  *    
+ * 
+ *  ---------------------- OUTPUT & INPUT Pin table ---------------------
+ *  +---------------+-------------------------+-------------------------+
+ *  |   Board       | INPUT Pin               | OUTPUT Pin              |
+ *  |               | Zero-Cross              |                         |
+ *  +---------------+-------------------------+-------------------------+
+ *  | Lenardo       | D7 (NOT CHANGABLE)      | D0-D6, D8-D13           |
+ *  +---------------+-------------------------+-------------------------+
+ *  | Mega          | D2 (NOT CHANGABLE)      | D0-D1, D3-D70           |
+ *  +---------------+-------------------------+-------------------------+
+ *  | Uno           | D2 (NOT CHANGABLE)      | D0-D1, D3-D20           |
+ *  +---------------+-------------------------+-------------------------+
+ *  | ESP8266       | D1(IO5),    D2(IO4),    | D0(IO16),   D1(IO5),    |
+ *  |               | D5(IO14),   D6(IO12),   | D2(IO4),    D5(IO14),   |
+ *  |               | D7(IO13),   D8(IO15),   | D6(IO12),   D7(IO13),   |
+ *  |               |                         | D8(IO15)                |
+ *  +---------------+-------------------------+-------------------------+
+ *  | ESP32         | 4(GPI36),   6(GPI34),   | 8(GPO32),   9(GP033),   |
+ *  |               | 5(GPI39),   7(GPI35),   | 10(GPIO25), 11(GPIO26), |
+ *  |               | 8(GPO32),   9(GP033),   | 12(GPIO27), 13(GPIO14), |
+ *  |               | 10(GPI025), 11(GPIO26), | 14(GPIO12), 16(GPIO13), |
+ *  |               | 12(GPIO27), 13(GPIO14), | 23(GPIO15), 24(GPIO2),  |
+ *  |               | 14(GPIO12), 16(GPIO13), | 25(GPIO0),  26(GPIO4),  |
+ *  |               | 21(GPIO7),  23(GPIO15), | 27(GPIO16), 28(GPIO17), |
+ *  |               | 24(GPIO2),  25(GPIO0),  | 29(GPIO5),  30(GPIO18), |
+ *  |               | 26(GPIO4),  27(GPIO16), | 31(GPIO19), 33(GPIO21), |
+ *  |               | 28(GPIO17), 29(GPIO5),  | 34(GPIO3),  35(GPIO1),  |
+ *  |               | 30(GPIO18), 31(GPIO19), | 36(GPIO22), 37(GPIO23), |
+ *  |               | 33(GPIO21), 35(GPIO1),  |                         |
+ *  |               | 36(GPIO22), 37(GPIO23), |                         |
+ *  +---------------+-------------------------+-------------------------+
+ *  | Arduino M0    | D7 (NOT CHANGABLE)      | D0-D6, D8-D13           |
+ *  | Arduino Zero  |                         |                         |
+ *  +---------------+-------------------------+-------------------------+
+ *  | Arduino Due   | D0-D53                  | D0-D53                  |
+ *  +---------------+-------------------------+-------------------------+
+ *  | STM32         | PA0-PA15,PB0-PB15       | PA0-PA15,PB0-PB15       |
+ *  | Black Pill    | PC13-PC15               | PC13-PC15               |
+ *  | BluePill      |                         |                         |
+ *  | Etc...        |                         |                         |
+ *  +---------------+-------------------------+-------------------------+
+ *  
+ *  
+ *  Work for dimmer on Domoticz or Web command :
+ *  http://URL/?POWER=XX 
+ *  0 -> 99 
+ *  more than 99 = 99  
+ *    
+ *  Update 2019 04 28 
+ *  correct issue full power for many seconds at start 
+ *  Update 2020 01 13 
+ *  Version 2 with cute web interface
+ *  V2.1    with temperature security ( dallas 18b20 ) 
+ *          MQTT to Domoticz for temp 
+ */
+
+
+
+
+/***************************
+ * Librairy
+ **************************/
+
+// time librairy   
+
+// Dimmer librairy 
+#include <RBDdimmer.h>   /// the corrected librairy  in RBDDimmer-master-corrected.rar , the original has a bug
+// Web services
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWiFiManager.h>    
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESP8266HTTPClient.h> 
+// File System
+#include <fs.h>
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include <ArduinoJson.h> // ArduinoJson : https://github.com/bblanchon/ArduinoJson
+// ota mise à jour sans fil
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+// Dallas 18b20
+//mqtt
+#include <PubSubClient.h>
+#define mqtt_user "Dimmer"
+//const char* mqtt_server = "192.168.1.20";
+//#define IDX 61
+//#define idxalarme 100   //beta
+
+/***************************
+ * Begin Settings
+ **************************/
+
+const String VERSION = "Version 2.2" ;
+
+/***************************
+ * temperature de sécurité
+ **************************/
+//#define MAXTEMP 75
+
+// WIFI
+// At first launch, create wifi network 'dimmer'  ( pwd : dimmer ) 
+
+//***********************************
+//************* Gestion du serveur WEB
+//***********************************
+// Create AsyncWebServer object on port 80
+WiFiClient domotic_client;
+// mqtt
+PubSubClient client(domotic_client);
+
+AsyncWebServer server(80);
+DNSServer dns;
+HTTPClient http;
+
+//////////
+
+int puissance = 0 ;
+int change = 0; 
+
+//***********************************
+//************* Time
+//***********************************
+//const long utcOffsetInSeconds = 3600;
+//char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+//WiFiUDP ntpUDP;
+//NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+int timesync = 0; 
+int timesync_refresh = 120; 
+
+// *************************************
+
+//#define USE_SERIAL  SerialUSB //Serial for boards whith USB serial port
+#define USE_SERIAL  Serial
+#define outputPin  0 
+#define zerocross  2 // for boards with CHANGEBLE input pins
+
+
+/***************************
+ * End Settings
+ **************************/
+
+//***********************************
+//************* Gestion de la configuration
+//***********************************
+
+struct Config {
+  char hostname[15];
+  int port;
+  String IDX;
+  String IDXAlarme;
+  int maxtemp;
+  char Publish[100];
+  
+};
+
+const char *filename_conf = "/config.json";
+Config config; 
+
+//***********************************
+//************* Gestion de la configuration - Lecture du fichier de configuration
+//***********************************
+
+// Loads the configuration from a file
+void loadConfiguration(const char *filename, Config &config) {
+  // Open file for reading
+  File configFile = SPIFFS.open(filename_conf, "r");
+
+  // Allocate a temporary JsonDocument
+  // Don't forget to change the capacity to match your requirements.
+  // Use arduinojson.org/v6/assistant to compute the capacity.
+  StaticJsonDocument<512> doc;
+
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, configFile);
+  if (error)
+    Serial.println(F("Failed to read file, using default configuration"));
+
+  // Copy values from the JsonDocument to the Config
+  config.port = doc["port"] | 1883;
+  strlcpy(config.hostname,                  // <- destination
+          doc["hostname"] | "192.168.1.20", // <- source
+          sizeof(config.hostname));         // <- destination's capacity
+  config.IDX = doc["IDX"] | 61; 
+  config.IDXAlarme = doc["IDXAlarme"] | 100; 
+  config.maxtemp = doc["maxtemp"] | 75; 
+  strlcpy(config.Publish,                  // <- destination
+          doc["Publish"] | "domoticz/in", // <- source
+          sizeof(config.Publish));         // <- destination's capacity
+  configFile.close();
+      
+}
+
+//***********************************
+//************* Gestion de la configuration - sauvegarde du fichier de configuration
+//***********************************
+
+void saveConfiguration(const char *filename, const Config &config) {
+  
+  // Open file for writing
+   File configFile = SPIFFS.open(filename_conf, "w");
+  if (!configFile) {
+    Serial.println(F("Failed to open config file for writing"));
+    return;
+  }
+
+  // Allocate a temporary JsonDocument
+  // Don't forget to change the capacity to match your requirements.
+  // Use arduinojson.org/assistant to compute the capacity.
+  StaticJsonDocument<512> doc;
+
+  // Set the values in the document
+  doc["hostname"] = config.hostname;
+  doc["port"] = config.port;
+  doc["IDX"] = config.IDX;
+  doc["IDXAlarme"] = config.IDXAlarme;
+  doc["maxtemp"] = config.maxtemp;
+  doc["Publish"] = config.Publish;
+  
+
+
+  // Serialize JSON to file
+  if (serializeJson(doc, configFile) == 0) {
+    Serial.println(F("Failed to write to file"));
+  }
+
+  // Close the file
+  configFile.close();
+}
+ 
+
+/***************************
+ * init Dimmer
+ **************************/
+
+dimmerLamp dimmer(outputPin, zerocross); //initialase port for dimmer for ESP8266, ESP32, Arduino due boards
+//dimmerLamp dimmer(outputPin); //initialase port for dimmer for MEGA, Leonardo, UNO, Arduino M0, Arduino Zero
+
+int outVal = 0;
+
+    //***********************************
+    //************* function web 
+    //***********************************
+const char* PARAM_INPUT_1 = "POWER"; /// paramettre de retour sendmode
+
+
+String getState() {
+  String state; 
+  int pow=dimmer.getPower(); 
+  state = String(pow)  ; 
+  return String(state);
+}
+
+
+String processor(const String& var){
+   Serial.println(var);
+  if (var == "STATE"){
+    return getState();
+  } 
+    if (var == "VERSION"){
+    return (VERSION);
+  } 
+
+} 
+ 
+
+
+String getconfig() {
+  String configweb;  
+  configweb = String(config.hostname) + ";" +  config.port + ";"  + config.IDX + ";"  + config.Publish;
+  return String(configweb);
+}
+
+    //***********************************
+    //************* Setup 
+    //***********************************
+
+void setup() {
+  Serial.begin(115200);
+
+  /// Correction issue full power at start
+  pinMode(outputPin, OUTPUT); 
+  digitalWrite(outputPin, LOW);
+
+  //démarrage file system
+  SPIFFS.begin();
+  Serial.println("Demarrage file System");
+  
+  // configuration dimmer
+  dimmer.begin(NORMAL_MODE, ON); //dimmer initialisation: name.begin(MODE, STATE) 
+  dimmer.setPower(outVal); 
+  
+  USE_SERIAL.println("Dimmer Program is starting...");
+
+      //***********************************
+    //************* Setup -  récupération du fichier de configuration
+    //***********************************
+  
+  // Should load default config if run for the first time
+  Serial.println(F("Loading configuration..."));
+  loadConfiguration(filename_conf, config);
+
+  // Create configuration file
+  Serial.println(F("Saving configuration..."));
+  saveConfiguration(filename_conf, config);
+
+  
+
+    //***********************************
+    //************* Setup - Connexion Wifi
+    //***********************************
+
+  AsyncWiFiManager wifiManager(&server,&dns);
+  wifiManager.autoConnect("dimmer","dimmer");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //Si connexion affichage info dans console
+  Serial.println("");
+  Serial.print("Connection ok sur le reseau :  ");
+ 
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP()); 
+  Serial.println(ESP.getResetReason());
+
+    //***********************************
+    //************* Setup - OTA 
+    //***********************************
+
+   ArduinoOTA.setHostname("PV Dimmer"); 
+   ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
+    //***********************************
+    //************* Setup - Web pages
+    //***********************************
+   
+   server.on("/",HTTP_ANY, [](AsyncWebServerRequest *request){
+    
+    if (request->hasParam(PARAM_INPUT_1)) { 
+      puissance = request->getParam(PARAM_INPUT_1)->value().toInt();  
+      change=1; 
+      request->send_P(200, "text/plain", getState().c_str());  
+      
+      }
+    else   request->send(SPIFFS, "/index.html", String(), false, processor);
+
+    
+  }); 
+
+  server.on("/state", HTTP_ANY, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", getState().c_str());
+  });
+
+
+  server.on("/all.min.css", HTTP_ANY, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/all.min.css", "text/css");
+  });
+
+    server.on("/favicon.ico", HTTP_ANY, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/favicon.ico", "image/png");
+  });
+
+  server.on("/fa-solid-900.woff2", HTTP_ANY, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/fa-solid-900.woff2", "text/css");
+  });
+  
+    server.on("/sb-admin-2.js", HTTP_ANY, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/sb-admin-2.js", "text/javascript");
+  });
+
+  server.on("/sb-admin-2.min.css", HTTP_ANY, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/sb-admin-2.min.css", "text/css");
+  });
+
+  server.on("/config.json", HTTP_ANY, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/config.json", "application/json");
+  });
+
+/////////////////////////
+////// mise à jour parametre d'envoie vers domoticz et récupération des modifications de configurations
+/////////////////////////
+
+server.on("/set", HTTP_ANY, [] (AsyncWebServerRequest *request) {
+      ///   /set?paramettre=xxxx
+    if (request->hasParam("save")) { Serial.println(F("Saving configuration..."));
+                          saveConfiguration(filename_conf, config);   
+                            }
+                          
+   if (request->hasParam("hostname")) { request->getParam("hostname")->value().toCharArray(config.hostname,15);  }
+   if (request->hasParam("port")) { config.port = request->getParam("port")->value().toInt();}
+   if (request->hasParam("Publish")) { request->getParam("Publish")->value().toCharArray(config.Publish,100);}
+   request->send(200, "text/html", getconfig().c_str());
+
+  });
+
+
+
+
+
+
+    //***********************************
+    //************* Setup -  demarrage du webserver et affichage de l'oled
+    //***********************************
+  Serial.println("start server");
+  server.begin(); 
+  
+ // Serial.println("start ntp");
+ // timeClient.begin();
+ // timeClient.update();
+  
+  
+
+
+  client.connect("Dimmer");
+  client.setServer(config.hostname, 1883);
+  
+
+}
+
+void loop() {
+
+  /// Changement de la puissance (  pb de Exception 9 si call direct ) 
+  if ( change == 1  ) {
+
+  dimmer.setPower(puissance); 
+  change = 0; 
+  }
+
+
+///  changement de la puissance
+
+
+
+ ArduinoOTA.handle();
+ delay(500); 
+}
+
+
+  
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+
+void mqtt(String idx, String value)
+{
+  String nvalue = "0" ; 
+  if ( value != "0" ) { nvalue = "2" ; }
+String message = "  { \"idx\" : " + idx +" ,   \"svalue\" : \"" + value + "\",  \"nvalue\" : " + nvalue + "  } ";
+
+  if (!client.connected()) {
+    reconnect();
+  }
+client.loop();
+  client.publish(config.Publish, String(message).c_str(), true);
+  
+}
+
+
+
